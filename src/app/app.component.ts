96d3b49c775881c5d869cdfea29980fa9e911b08
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-02-05 17:19:59 -0300

    exibindo dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,13 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public todos: any[] = []; //variavel no angular
+  public todos: Todo[] = []; //variavel no angular
+  public title: String = 'Minhas Tarefas';
 
   
   constructor() {
-   this.todos.push('passear com o meu cachorro')
-   this.todos.push('corta o cabelo')
-   this.todos.push('ir ao supermercado')
+   this.todos.push(new Todo(1 , 'ler o livro', false));
+   this.todos.push(new Todo(2, 'estudar java', false));
+   this.todos.push(new Todo(3, 'ir correr', true));
     
   }
 }
