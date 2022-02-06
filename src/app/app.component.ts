70e0217d29b29ddc9895b8e4e7cad3fe090af86c
Alvaro Rodrigues <alvim.lopez@gmail.com>
2022-02-06 17:39:28 -0300

    Adicionando Forms Basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,9 +10,20 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = []; //variavel no angular
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
+ 
+
 
   
-  constructor() {
+  constructor(private fb:FormBuilder) {
+    this.form = this.fb.group({
+      title: ['Insira sua tarefa', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
    this.todos.push(new Todo(1 , 'ler o livro', false));
    this.todos.push(new Todo(2, 'estudar java', false));
    this.todos.push(new Todo(3, 'ir correr', true)); 
